36dd0c169fc6efd44b9b5b10d740b1dcfcd8e57c
navitha <navithareddy112@gmail.com>
2022-09-27 20:24:56 +0530

    [navitha] UC6MoodAnalyzer

--- MoodAnalyser/UC5MoodAnalyserFactory.cs
+++ MoodAnalyser/UC5MoodAnalyserFactory.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MoodAnalyser
 {
-    internal class UC5MoodAnalyser
+    public class UC5MoodAnalyserFactory
     {
+       
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+
+            if(type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if(type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new object[] {message});
+                    return ctor.Invoke(null);
+                }
+
+                else
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+                }
+
+            }
+            else
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "class is not found");
+            }
+        }
     }
 }

765e5d62fa3c97211e80de099be6373b76d8c8dc
navitha <navithareddy112@gmail.com>
2022-09-27 18:36:35 +0530

    [navitha] UC3MoodAnalyser

---
+++ MoodAnalyser/UC3MoodAnalyser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class UC3MoodAnalyser
+    {
+        public string message;
+        public class MoodAnalysisException : Exception
+        {
+            public MoodAnalysisException(string msg) : base(msg)
+            {
+
+            }
+        }                        
+        public string AnalyseMood(string message)
+        {
+            try
+            {
+                if (message == null)
+                {
+                    throw (new MoodAnalysisException("Null Mood"));
+                }
+                else
+                {
+                    throw (new MoodAnalysisException("Empty Mood"));
+                }
+            }
+            catch (MoodAnalysisException ex)
+            {
+                return ex.Message;
+            }
+        }
+
+      
+    }
+}
---
+++ MoodAnalyser/UC3MoodAnalyserHelpers.cs
@@ -0,0 +1,25 @@
+﻿namespace MoodAnalyser
+{
+    internal static class UC3MoodAnalyserHelpers
+    {
+
+        public static string AnalyseMood(string message)
+        {
+            try
+            {
+                if (message == null)
+                {
+                    throw (new MoodAnalysisException("Null mood"));
+                }
+                else
+                {
+                    throw (new MoodAnalysisException("Empty Mood"));
+                }
+            }
+            catch (MoodAnalysisException ex)
+            {
+                rerurn message;
+            }
+        }
+    }
+}

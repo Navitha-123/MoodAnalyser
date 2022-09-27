94fa4919c9f85496d38e6e86d73cef76ece52c4f
navitha <navithareddy112@gmail.com>
2022-09-27 16:49:25 +0530

    [navitha] UC2MoodAnalyser

---
+++ MoodAnalyser/UC1HappyOrSad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class UC1HappyOrSad
+    {
+        public string AnalyseMood(string message)
+        {
+            message = message.ToLower();
+            if (message.Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
---
+++ MoodAnalyser/UC2MoodAnalyser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class UC2MoodAnalyser
+    {
+
+        public string message;
+        public string MoodAnalyse(string message)
+        {
+            try
+            {
+                message = message.ToLower();
+                if (message.Contains("happy"))
+                {
+                    return "happy";
+
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "happy";
+            }
+        }
+    }
+}

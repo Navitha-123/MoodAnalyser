a901ad5e187d086c339acc7ee51ab89e6fc5a513
navitha <navithareddy112@gmail.com>
2022-09-27 20:59:55 +0530

    [navitha] UC7MoodAnalyserFactory

--- MoodAnalyser/UC5MoodAnalyserFactory.cs
+++ MoodAnalyser/UC5MoodAnalyserFactory.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using static MoodAnalyser.UC3MoodAnalyser;
 
 
 namespace MoodAnalyser
@@ -12,15 +14,18 @@ namespace MoodAnalyser
     public class UC5MoodAnalyserFactory
     {
        
+
+
+
         public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
         {
-            Type type = typeof(MoodAnalyser);
+            Type type = typeof(UC4MoodAnalyser);
 
             if(type.Name.Equals(className) || type.FullName.Equals(className))
             {
                 if(type.Name.Equals(constructorName))
                 {
-                    ConstructorInfo ctor = type.GetConstructor(new object[] {message});
+                    ConstructorInfo ctor = type.GetConstructor((Type[])new object[] {message});
                     return ctor.Invoke(null);
                 }
 
--- MoodAnalyser/UC6MoodAnalyzer.cs
+++ MoodAnalyser/UC6MoodAnalyzer.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using static MoodAnalyser.UC3MoodAnalyser;
 
 namespace MoodAnalyser
 {
-    internal class UC6MoodAnalyzer
+    public class UC6MoodAnalyzer
     {
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] {typeof(string)});
+                    object instance = ctor.Invoke(new object[] {message});  
+                    return instance;
+                }
+
+                else
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+                }
+
+            }
+            else
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "class is not found");
+            }
+        }
+
+        public static string InvokeAnalysisMood(string message,string MethodName)
+        {
+            try
+            {
+                Type type = Type.GetType(MoodAnalyserProblem.MoodAnalyser);
+                object moodAnalyseObject = MoodAnalyserFactoryUCSix.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserProblem.MoodAnalyser", "MoodAnalyser", message);
+                MethodInfo analyseMoodInfo = type.GetMethod(MethodName);
+                object mood = analyseMoodInfo.Invoke(moodAnalyseObject, null);
+            }
+            catch(NullReferenceException)
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Method is not found");
+            }
+        }
     }
 }
